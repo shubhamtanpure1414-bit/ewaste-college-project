123b1f1bfa98e263f7c794a5b53766e7f0c8ec0a
shubhamtanpure1414-bit <shubhamtanpure1414@gmail.com>
2026-04-26 13:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_db():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT")
+        port=int(os.getenv("MYSQLPORT"))
     )
 # ─────────────────────────── HELPERS ───────────────────────────
 def login_required(f):
